edfd00d3f3789c4465e84f08ce9b5f8e29e81510
prayogsuns <prayogsuns@gmail.com>
2021-01-13 17:29:05 +0530

    quiz1 version 1.1.0

--- quiz1/main.go
+++ quiz1/main.go
@@ -14,12 +14,12 @@ var flagSetName string = os.Args[0]
 var stop = make(chan time.Duration)
 
 func usage() (string, int) {
-  fs := flag.NewFlagSet(flagSetName, flag.ExitOnError)
+	fs := flag.NewFlagSet(flagSetName, flag.ExitOnError)
 	fcsv := fs.String("csv", "quiz.csv", "a csv file in the format of 'question.answer'")
 	flimit := fs.Int("limit", 30, "the time limit of quiz in seconds")
 
 	//ferror := fs.Parse(os.Args[1:])
-  fs.Parse(os.Args[1:]) 
+	fs.Parse(os.Args[1:])
 	//fmt.Println("XYZ")
 	//fmt.Println(fmt.Print(ferror))
 	//fmt.Println("AAA")
@@ -31,7 +31,7 @@ func main() {
 	csv, limit := usage()
 	//fmt.Println(csv, limit)
 	if (csv == "-limit") || (csv == "--limit") {
-    fmt.Println("flag csv can't be set to -limit|--limit")
+		fmt.Println("flag csv can't be set to -limit|--limit")
 		os.Exit(1)
 	}
 	//fmt.Println("BBB")
@@ -44,11 +44,16 @@ func main() {
 
 	r := quiz.ReadQuiz(f)
 
-  startQuiz()
-  
+	startQuiz()
+
 	startTime := time.Now()
 	go watchTime(stop, startTime, limit)
-	quiz.WriteQuiz(r, 0, stop, limit)
+	//quiz.WriteQuiz(r, 0, stop, limit)
+	go quiz.WriteQuiz(r, 0, limit)
+
+	if time_left := <-stop; time_left == time.Duration(0) {
+		quiz.PrintResult()
+	}
 
 	if err := f.Close(); err != nil {
 		log.Fatal(err)
@@ -63,35 +68,34 @@ func startQuiz() {
 	var start []byte = make([]byte, 1)
 	_, start_err := fmt.Scanln(&start)
 	if start_err != nil {
-    log.Fatal(start_err)
+		log.Fatal(start_err)
 	}
 
 	bad_start := false
 	if len(start) != 1 {
-	  bad_start = true
+		bad_start = true
 	} else {
-    if !bytes.Equal(bytes.ToLower(start), []byte("y")) {
-	    bad_start = true
+		if !bytes.Equal(bytes.ToLower(start), []byte("y")) {
+			bad_start = true
 		}
 	}
 
 	if bad_start {
 		fmt.Println("Wrong Input! Expected y|Y. Exiting...")
-    os.Exit(1)
+		os.Exit(1)
 	}
 
 }
 
 func watchTime(stop chan time.Duration, start time.Time, limit int) {
 	for {
-	  t := time.Now()
-	  elapsed := t.Sub(start)
+		t := time.Now()
+		elapsed := t.Sub(start)
 		//fmt.Println(elapsed, (time.Duration(limit) * time.Second))
-	  if elapsed >= (time.Duration(limit) * time.Second) {
-		  stop <- time.Duration(0)
-	  } else {
-      stop <- elapsed 
-		}
-  } 
+		if elapsed >= (time.Duration(limit) * time.Second) {
+			stop <- time.Duration(0)
+		} // else {
+		//  stop <- elapsed
+		//}
+	}
 }
-
--- quiz1/traps/quiz1.go
+++ quiz1/traps/quiz1.go
@@ -3,73 +3,59 @@ package quiz
 import (
 	"encoding/csv"
 	"fmt"
-	//"io"
 	"log"
 	"os"
 	"strconv"
 	"strings"
-	"time"
 )
 
-func ReadQuiz(f *os.File) *csv.Reader {
-	r := csv.NewReader(f)
-	return r
-}
+var results = make([]int, 0, 1000)
+var total_quiz_count int
 
-func WriteQuiz(r *csv.Reader, rf int, stop chan time.Duration, limit int) {
+func ReadQuiz(f *os.File) [][]string {
+	r := csv.NewReader(f)
 	records, err := r.ReadAll()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	results := make ([]int, len(records))
-
-	for i, _ := range records {
-    time_left := <-stop; 
-		//fmt.Println(time_left, time.Duration(0))
-		if time_left > time.Duration(0) {
-			record := records[i][rf]
-			fmt.Printf("Problem #%d: %s = ", i+1, record)
-			var sum int
-			_, scan_err := fmt.Scanln(&sum)
-			//fmt.Printf("%v, %T\n", sum, sum)
-			if scan_err != nil {
-				log.Fatal(scan_err)
-
-			}
-
-			real_sum, err_s2i := strconv.Atoi(strings.TrimSpace(records[i][rf+1]))
-			if err_s2i != nil {
-				log.Fatal(err_s2i)
-			}
-
-			updateResult(results, i, sum, real_sum)
-		}
-	}
-	printResult(results, records)
+	return records
 }
 
-func updateResult(results []int, index, sum, real_sum int) {
+func WriteQuiz(records [][]string, rf int, limit int) {
+	total_quiz_count = len(records)
+
+	for i, _ := range records {
+		record := records[i][rf]
+		fmt.Printf("Problem #%d: %s = ", i+1, record)
+		var sum int
+		_, scan_err := fmt.Scanln(&sum)
+		//fmt.Printf("%v, %T\n", sum, sum)
+		if scan_err != nil {
+			log.Fatal(scan_err)
+
+		}
+
+		real_sum, err_s2i := strconv.Atoi(strings.TrimSpace(records[i][rf+1]))
+		if err_s2i != nil {
+			log.Fatal(err_s2i)
+		}
+
+		updateResult(i, sum, real_sum)
+	}
+
+}
+
+func updateResult(index, sum, real_sum int) {
 	result := 0
 	if sum == real_sum {
 		result = 1
 	}
 
-	results[index] = result
+	results = append(results, result)
 }
 
-func printUniqueValue(arr []int, numv int) int {
-	//Create a   dictionary of values for each element
-	dict := make(map[int]int)
-	for _, num := range arr {
-		dict[num] = dict[num] + 1
-	}
-	return dict[numv]
-}
-
-func printResult(results []int, records [][]string) {
-	total_quiz_count := len(records)
-	total_correct := printUniqueValue(results, 1)
+func PrintResult() {
+	total_correct := len(results)
 	fmt.Println("\nResult = ", total_correct, "/", total_quiz_count)
 }
-
