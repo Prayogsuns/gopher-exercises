9572e9582736c3ce7278d3b0e0d3e794a58876b5
prayogsuns <prayogsuns@gmail.com>
2021-03-20 20:44:23 +0530

    urlshort version 1.0.2

--- urlshort/main/main.go
+++ urlshort/main/main.go
@@ -4,11 +4,11 @@ import (
 	"database/sql"
 	"flag"
 	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/prayogsuns/gopher-exercises/urlshort"
 	"io/ioutil"
 	"net/http"
 	"os"
-	"github.com/prayogsuns/gopher-exercises/urlshort"
-	_ "github.com/mattn/go-sqlite3"
 	"strconv"
 )
 
@@ -67,17 +67,20 @@ func main() {
 		http.ListenAndServe(":8081", serverHandler)
 	} else if *dbopt {
 		fmt.Println("Db Actions")
-    var DB_NAME = "sqllite3db"
-    var filename = DB_NAME + ".db"
-    var table = map[string]string{"name":"urlmap", "col1": "id", "col2": "path", "col3": "url"}
-    
-    db := createDb(filename) 
-    createTable(db, table) 
-    insertDataInDb(db, table) 
+		var DB_NAME = "sqllite3db"
+		var filename = DB_NAME + ".db"
+		var table = map[string]string{"name": "urlmap", "col1": "id", "col2": "path", "col3": "url"}
 
-		queryDb(db, table)
+		db := createDb(filename)
+		createTable(db, table)
+		insertDataInDb(db, table)
+
+		dbMapHandler := urlshort.MapHandler(queryDb(db, table), mapHandler)
 		closeDb(filename, db)
 		fmt.Println("Db Closed")
+
+		fmt.Println("Starting the server on :8082")
+		http.ListenAndServe(":8082", dbMapHandler)
 	}
 	//http.ListenAndServe(":8080", mapHandler)
 }
@@ -99,37 +102,41 @@ func check(e error) {
 }
 
 func createDb(filename string) *sql.DB {
-  db, err := sql.Open("sqlite3", filename)
+	db, err := sql.Open("sqlite3", filename)
 	check(err)
-  return db
+	return db
 }
 
 func createTable(db *sql.DB, table map[string]string) {
 	stmt, err := db.Prepare("CREATE TABLE IF NOT EXISTS " + table["name"] + " (" + table["col1"] + " INTEGER PRIMARY KEY, " + table["col2"] + " TEXT, " + table["col3"] + " TEXT)")
 	check(err)
-  stmt.Exec()
+	stmt.Exec()
 }
 
 func insertDataInDb(db *sql.DB, table map[string]string) {
-  stmt, err := db.Prepare("INSERT INTO " + table["name"] + " (" + table["col1"] +", " + table["col2"] + ", " + table["col3"] + ") VALUES (?, ?, ?)")
+	stmt, err := db.Prepare("INSERT INTO " + table["name"] + " (" + table["col1"] + ", " + table["col2"] + ", " + table["col3"] + ") VALUES (?, ?, ?)")
 	check(err)
 	stmt.Exec(001, "/urlshort", "https://github.com/gophercises/urlshort")
 	stmt.Exec(002, "/urlshort-final", "https://github.com/gophercises/urlshort/tree/solution")
 }
 
-func queryDb(db *sql.DB, table map[string]string) {
-  rows, err := db.Query("SELECT " + table["col1"] + ", " + table["col2"] + ", " + table["col3"] + " from " + table["name"]) 
+func queryDb(db *sql.DB, table map[string]string) map[string]string {
+	path_map := map[string]string{}
+
+	rows, err := db.Query("SELECT " + table["col1"] + ", " + table["col2"] + ", " + table["col3"] + " from " + table["name"])
 	check(err)
 	var id int
 	var path string
 	var url string
 	for rows.Next() {
-    rows.Scan(&id, &path, &url)
+		rows.Scan(&id, &path, &url)
 		fmt.Println(strconv.Itoa(id) + ": " + path + " " + url)
+		path_map[path] = url
 	}
+	return path_map
 }
 
 func closeDb(filename string, db *sql.DB) {
-	db.Close() 
-  os.Remove(filename)
+	db.Close()
+	os.Remove(filename)
 }
